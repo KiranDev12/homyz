1e69354f6201272914ea9586f13c21a45d2d2527
Kiran Dev K <kirandev.edu@gmail.com>
2023-09-26 10:08:20 +0530

    hero section done

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Companies.css";
+function Companies() {
+  return (
+    <section className="c-wrapper">
+      <div className="padding innerWidth flexCenter c-container">
+        <img src="./prologis.png" alt="" srcset="" />
+        <img src="./tower.png" alt="" srcset="" />
+        <img src="./equinix.png" alt="" srcset="" />
+        <img src="./realty.png" alt="" srcset="" />
+      </div>
+    </section>
+  );
+}
+
+export default Companies;
